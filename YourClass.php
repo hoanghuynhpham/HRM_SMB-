4d193b3fd43d9e16838940a70798fb17f5d00aa6
Phạm Hoàng Huynh <huynh.pham@smbplus.vn>
2025-03-17 13:27:50 +0700

    hh

---
+++ YourClass.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Phamhoanghuynh\HrmSmb;
+
+class YourClass {
+    public function someMethod() {
+        return "This is some method";
+    }
+}
--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require __DIR__ . '/vendor/autoload.php'; // Nạp autoload của Composer
-
-use Phamhoanghuynh\HrmSmb\MyPackage; // Import class
-
-$package = new MyPackage();
-echo $package->hello(); // Gọi phương thức hello()
