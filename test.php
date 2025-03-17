02eb742f0d227e7fd72ef92b2aa15fefb8312002
Phạm Hoàng Huynh <huynh.pham@smbplus.vn>
2025-03-17 09:11:21 +0700

    commit

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
 
-require 'vendor/autoload.php'; 
+require __DIR__ . '/vendor/autoload.php'; // Nạp autoload của Composer
 
-use Phamhoanghuynh\HrmSmb\MyPackage; 
+use Phamhoanghuynh\HrmSmb\MyPackage; // Import class
 
-$package = new MyPackage(); 
-echo $package->hello(); 
+$package = new MyPackage();
+echo $package->hello(); // Gọi phương thức hello()
